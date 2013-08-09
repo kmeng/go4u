28834c7c32601eee9bda82d9515a506ed19a403f
Kangjian Meng <kangjian.meng@gmail.com>
2013-08-09 11:34:03 +0800

    Combine all parser and parser-domain into one file.

--- go4u/src/main/java/com/go4u/parser/ParserFactory.java
+++ go4u/src/main/java/com/go4u/parser/ParserFactory.java
@@ -1,16 +1,14 @@
 package com.go4u.parser;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ParserFactory implements InitializingBean {
@@ -24,36 +22,31 @@ public class ParserFactory implements InitializingBean {
 	}
 
 	public void afterPropertiesSet() throws Exception {
-        Map<String, ProductParser> parserMap = initializingParserMap();
-        BufferedReader domainParserMapReader = new BufferedReader(new InputStreamReader(ParserFactory.class.getResourceAsStream("/parser/domain-parser")));
-        String domainParserMapLine = null;
-        while((domainParserMapLine = domainParserMapReader.readLine()) != null){
-            String[] domainParserNamePair = domainParserMapLine.split("=");
-            String parserName = domainParserNamePair[0];
-            String[] domains = domainParserNamePair[1].split(",");
-            for(String domain : domains){
-                domainRuleMap.put(domain, parserMap.get(parserName));
+        BufferedReader parserDefinitionReader = new BufferedReader(
+                new InputStreamReader(ParserFactory.class.getResourceAsStream("/parser/parsers")));
+        String ruleLine;
+        while((ruleLine = parserDefinitionReader.readLine()) != null){
+            if(ruleLine.startsWith("[") && ruleLine.endsWith("]")){
+                String[] domains = ruleLine.replace("[", "").replace("]", "").split(",");
+                ProductParser productParser = parseRuleDefinition(parserDefinitionReader);
+                for(String domain : domains){
+                    domainRuleMap.put(domain, productParser);
+                }
             }
+
         }
 	}
 
-    private Map<String, ProductParser> initializingParserMap() throws Exception{
-        Map<String, ProductParser> parserMap = new HashMap<String, ProductParser>();
-        File parserFolder = new File(ParserFactory.class.getResource("/parser").getPath());
-        File[] parserFiles = parserFolder.listFiles();
-        for(File parserFile : parserFiles){
-            if(parserFile.getName().startsWith("parser.")){
-                BufferedReader br = new BufferedReader(new FileReader(parserFile));
-                String rule = null;
-                List<String> ruleList = new ArrayList<String>();
-                while((rule = br.readLine()) != null){
-                    ruleList.add(rule);
-                }
-//                parserMap.put(parserFile.getName(), new ProductParserImpl(ruleList));
-                parserMap.put(parserFile.getName(), new ProductParserJSoupImpl(ruleList));
-            }
+    private ProductParser parseRuleDefinition(BufferedReader parserDefinitionReader) throws Exception{
+        List<String> ruleList = new ArrayList<String>();
+        String rule;
+        while((rule = parserDefinitionReader.readLine()) != null){
+            if(rule.trim().equals(""))
+                break;
+
+            ruleList.add(rule);
         }
 
-        return parserMap;
+        return new ProductParserJSoupImpl(ruleList);
     }
 }
